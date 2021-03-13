07f19ec97350a940a45609438e4f2ab77cfc8e01
atsushi-kitazawa <atsushi.kitazawa.1987@gmail.com>
2021-03-13 14:17:59 +0900

    Add Singleton Pattern Example.

---
+++ src_singleton/gof_design_pattern/creation/singleton/Singleton.java
@@ -0,0 +1,12 @@
+package gof_design_pattern.creation.singleton;
+
+public class Singleton {
+	private static Singleton instance = new Singleton();
+
+	private Singleton() {
+	}
+
+	public static Singleton getInstance() {
+		return instance;
+	}
+}
